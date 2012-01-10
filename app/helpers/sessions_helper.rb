13e7cca3de363289f3927e92c731028ccbadc386
Tushar Garg <fuzorblade@gmail.com>
2012-01-09 21:28:58 -0500

    added friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,7 @@ module SessionsHelper
 	end
 
     def deny_access
+			store_location
       redirect_to(signin_path, :notice => "Please sign in to access this page.")
     end
 
@@ -30,7 +31,20 @@ module SessionsHelper
     	user == current_user
     end
 
-	private
+    def redirect_back_or(default)
+			redirect_to(session[:return_to] || default)
+			clear_location
+    end
+
+private
+
+	def store_location
+		session[:return_to] = request.fullpath
+	end
+
+	def clear_location
+		session[:return_to] = nil
+	end
 
 	def user_from_remember_token
 		User.authenticate_with_salt(*remember_token)
