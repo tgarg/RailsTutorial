968c2ffee21cfe86a1054117249da562e5502e26
Tushar Garg <fuzorblade@gmail.com>
2012-03-19 10:19:47 -0400

    finished Chapter 11. Implemented user-microposts sans exercises

--- spec/controllers/microposts_controler_spec.rb
+++ spec/controllers/microposts_controler_spec.rb
@@ -63,4 +63,37 @@ describe MicropostsController do
 			end
 		end
 	end
+
+	describe "DELTE 'destroy'" do
+		
+		describe "for an un-authorized user" do
+			
+			before(:each) do
+				@user = Factory(:user)
+				@micropost = Factory(:micropost, :user => @user)
+				@wrong_user = Factory(:user, :email => Factory.next(:email))
+				test_sign_in(@wrong_user)
+			end
+
+			it "should deny access" do
+				delete :destroy, :id => @micropost
+				response.should redirect_to(root_path)
+			end
+		end
+
+		describe "for an authorized user" do
+			
+			before(:each) do
+				@user = Factory(:user)
+				@micropost = Factory(:micropost, :user => @user)
+				test_sign_in(@user)
+			end
+
+			it "should destroy the micropost" do
+				lambda do
+					delete :destroy, :id => @micropost
+				end.should change(Micropost, :count).by(-1)
+			end
+		end
+	end
 end
