d1e8b3581ce917986e8085fe8db76f294705d7b9
Tushar Garg <fuzorblade@gmail.com>
2012-03-23 19:19:33 -0400

    added follow/unfollow, however ajax implementation is broken

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,44 @@ require 'spec_helper'
 describe UsersController do
 	render_views
 
+  describe "follow pages" do
+
+    describe "when signed in" do
+
+      before(:each) do
+        @user=Factory(:user)
+        test_sign_in(@user)
+        @other_user=Factory(:user, :email => Factory.next(:email))
+        @user.follow!(@other_user)
+      end
+
+      it "should show users followers" do
+        get :followers, :id => @other_user
+        response.should have_selector("a", :href => user_path(@user),
+                                           :content => @user.name)
+      end
+
+      it "should show user's following" do
+        get :following, :id => @user
+        response.should have_selector("a", :href => user_path(@other_user),
+                                           :content => @other_user.name)
+      end
+    end  
+
+    describe "when not signed in" do
+      
+      it "should protect 'followers" do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+
+      it "should protect 'following" do
+        get :following, :id => 1
+        response.should redirect_to(signin_path)
+      end
+    end
+  end
+
   describe "GET 'index'" do
     
     describe "for non-signed in users" do
